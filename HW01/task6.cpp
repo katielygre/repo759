b9c3d5a251d9b6e95a4d02b221a0bc1a9e4bb31c
Katie Lygre <lygre@cnerg-docker-03.neep.wisc.edu>
2026-01-28 10:31:53 -0600

    fixed syntax error

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -6,15 +6,15 @@
 int main(int argc, char* argv[]) {          // integer retern type of main function, argc is argument count, argv is argument vector
     int N = atoi(argv[1]);                  // convert argument from string to integer
 
-    for (int i = 0, i <= N, i++){           // for loop from 0 to N
+    for (int i = 0; i <= N; i++) {           // for loop from 0 to N
         printf("%d ", i);                   // print the value of i followed by a space
     }
     printf("\n");                           // print a newline character after the loop
 
-    for (int i = N, i>= 0, i--){            // for loop from N to 0
+    for (int i = N; i>= 0; i--) {            // for loop from N to 0
         std::cout << i << " ";              // print the value of i followed by a space
     }
-    std::cout << std::endl                  // prints new line and flushes output
+    std::cout << std::endl;                 // prints new line and flushes output
 
     return 0;                               // ends program
 }

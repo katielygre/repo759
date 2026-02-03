9c5631a8ce29f55fc42178d691928b32dcd4bcf6
Katie Lygre <lygre@cnerg-docker-03.neep.wisc.edu>
2026-02-03 11:41:44 -0600

    add timing txt

--- HW02/task1.sh
+++ HW02/task1.sh
@@ -5,6 +5,6 @@
 #SBATCH -o task1.out -e task1.err
 #SBATCH --array=10-30
 
-n=$(python3 -c "print(2**${SLURM_ARRAY_TASK_ID})")
+n=$((2**SLURM_ARRAY_TASK_ID))
 
-./task1 $n
+./task1 $n > "timing_$SLURM_ARRAY_TASK_ID.txt"
